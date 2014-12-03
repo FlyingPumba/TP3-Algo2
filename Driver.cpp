e0f6b7cedc826e31f970b9c56039ed85ca06a866
Ivan Arcuschin <iarcuschin@gmail.com>
2014-12-03 02:08:28 -0300

    Algoritmos del Driver

--- Driver.cpp
+++ Driver.cpp
@@ -3,134 +3,221 @@
 
 namespace aed2 {
 
-Driver::Driver(const Conj<Estacion> &estacionesIniciales) : ciudad(NULL) {
-    Conj<Estacion>::const_Iterador it = estacionesIniciales.CrearIt();
-    while (it.HaySiguiente()) {
-    	mapa.Agregar(it.Siguiente());
-        it.Avanzar();
+    Driver::Driver(const Conj<Estacion> &estacionesIniciales) : ciudad(NULL) {
+        Conj<Estacion>::const_Iterador it = estacionesIniciales.CrearIt();
+        while (it.HaySiguiente()) {
+            mapa.Agregar(it.Siguiente());
+            it.Avanzar();
+        }
+        ciudad = new Ciudad(mapa);
     }
-    ciudad = new Ciudad(mapa);
-}
 
-Driver::~Driver() {
-    delete ciudad;
-}
-
-Nat Driver::CantidadEstaciones() const
-{
-	int cant = 0;
-	Conj<Estacion>::const_Iterador it = mapa.Estaciones();
-    while (it.HaySiguiente()) {
-    	cant = cant + 1;
-        it.Avanzar();
+    Driver::~Driver() {
+        delete ciudad;
     }
-    return cant;
-}
 
-Estacion IesimaEstacion(Nat i) const
-{
-	Conj<Estacion>::const_Iterador it = mapa.Estaciones();
-	int i = 0;
-    while (j < i) {
-    	j = j + 1;
-        it.Avanzar();
+    Nat Driver::CantidadEstaciones() const
+    {
+        int cant = 0;
+        Conj<Estacion>::const_Iterador it = mapa.Estaciones();
+        while (it.HaySiguiente()) {
+            cant = cant + 1;
+            it.Avanzar();
+        }
+        return cant;
     }
-    return it.Siguiente();
-}
 
-
-Nat Driver::CantidadDeSendasParaEstacion(const Estacion &e) const
-{
-	Conj<Estacion>::const_Iterador it = mapa.Estaciones();
-	int cant = 0;
-    while (it.HaySiguiente()) {
-    	if (mapa.Conectadas(e, it.Siguiente())) {
-    		cant = cant + 1;
-    	}
-
-        it.Avanzar();
+    Estacion IesimaEstacion(Nat i) const
+    {
+        Conj<Estacion>::const_Iterador it = mapa.Estaciones();
+        int j = 0;
+        while (j < i) {
+            j = j + 1;
+            it.Avanzar();
+        }
+        return it.Siguiente();
     }
-    return cant;
-}
 
-Estacion Driver::IesimaEstacionDeSenda(const Estacion &e, Nat i) const
-{
-	Conj<Estacion>::const_Iterador it = mapa.Estaciones();
-	int j = 0;
-    while (j < i) {
-    	if (mapa.Conectadas(e, it.Siguiente())) {
-    		j = j + 1;
-    	}
-        it.Avanzar();
+
+    Nat Driver::CantidadDeSendasParaEstacion(const Estacion &e) const
+    {
+        Conj<Estacion>::const_Iterador it = mapa.Estaciones();
+        int cant = 0;
+        while (it.HaySiguiente()) {
+            if (mapa.Conectadas(e, it.Siguiente())) {
+                cant = cant + 1;
+            }
+
+            it.Avanzar();
+        }
+        return cant;
     }
-    return it.Siguiente();
-}
 
-Restriccion Driver::IesimaRestriccionDeSenda(const Estacion &e1, Nat i) const
-{
-	Conj<Estacion>::const_Iterador it = mapa.Estaciones();
-	int j = 0;
-    while (j < i) {
-    	if (mapa.Conectadas(e, it.Siguiente())) {
-    		j = j + 1;
-    	}
-        it.Avanzar();
+    Estacion Driver::IesimaEstacionDeSenda(const Estacion &e, Nat i) const
+    {
+        Conj<Estacion>::const_Iterador it = mapa.Estaciones();
+        int j = 0;
+        while (j < i) {
+            if (mapa.Conectadas(e, it.Siguiente())) {
+                j = j + 1;
+            }
+            it.Avanzar();
+        }
+        return it.Siguiente();
     }
-    RestriccionTP& rest = mapa.Rest(it.Siguiente(), e);
-    return "";
-}
 
-void Driver::AgregarSenda(const Estacion &e1, const Estacion &e2, Restriccion r)
-{
-    ArbolSintactico* expr = ArbolSintactico::LeerDeString(r);
-    std::cout << expr->aString() << std::endl;
+    Restriccion Driver::IesimaRestriccionDeSenda(const Estacion &e1, Nat i) const
+    {
+        Conj<Estacion>::const_Iterador it = mapa.Estaciones();
+        int j = 0;
+        while (j < i) {
+            if (mapa.Conectadas(e, it.Siguiente())) {
+                j = j + 1;
+            }
+            it.Avanzar();
+        }
+        RestriccionTP& rest = mapa.Rest(it.Siguiente(), e);
+        return to_str(rest);
+    }
 
-    delete expr;
-}
+    void Driver::AgregarSenda(const Estacion &e1, const Estacion &e2, Restriccion r)
+    {
+        ArbolSintactico* expr = ArbolSintactico::LeerDeString(r);
+        std::cout << expr->aString() << std::endl;
 
-Nat Driver::CantidadRobotsActivos() const
-{
- // TODO
-}
+        RestriccionTP& rest = ParsearArbolSintactico(expr);
+        mapa.Conectar(e1, e2, rest);
 
-RUR Driver::IesimoRobotActivo(Nat i) const
-{
- // TODO
-}
+        if (ciudad != NULL) {
+            delete ciudad;
+        }
+        ciudad = new Ciudad(mapa);
+        delete expr;
+    }
 
-Estacion Driver::EstacionActualIesimoRobotActivo(Nat i) const
-{
- // TODO
-}
+    Nat Driver::CantidadRobotsActivos() const
+    {
+        int cant = 0;
+        itArreglo it = itArreglo(*robots);
+        while (it.HaySiguiente()) {
+            cant = cant + 1;
+            it.Avanzar();
+        }
+        return cant;
 
-Conj<Caracteristica> Driver::CaracteristicasIesimoRobotActivo(Nat i) const
-{
- // TODO
-}
+    }
 
-Nat Driver::CantInfraccionesIesimoRobotActivo(Nat i) const
-{
- // TODO
-}
+    RUR Driver::IesimoRobotActivo(Nat i) const
+    {
+        itArreglo it = itArreglo(*robots);
+        int j = 0;
+        while (j < i) {
+            j = j + 1;
+            it.Avanzar();
+        }
+        return it.Siguiente().posEstacion.rur;
+    }
 
-RUR Driver::ElMasInfractor() const
-{
- // TODO
-}
+    Estacion Driver::EstacionActualIesimoRobotActivo(Nat i) const
+    {
+        itArreglo it = itArreglo(*robots);
+        int j = 0;
+        while (j < i) {
+            j = j + 1;
+            it.Avanzar();
+        }
+        return (it.Siguiente()).estActual;
+    }
 
-void Driver::Entrar(const Conj<Caracteristica> &cs, const Estacion &estacionInicial)
-{
- // TODO
-}
+    Conj<Caracteristica> Driver::CaracteristicasIesimoRobotActivo(Nat i) const
+    {
+        itArreglo it = itArreglo(*robots);
+        int j = 0;
+        while (j < i) {
+            j = j + 1;
+            it.Avanzar();
+        }
+        Conj<Caracteristica> conj;
+        ConjRapido::const_Iterador itTags = (it.Siguiente()).tags.CrearIt();
+        while (itTags.HaySiguiente()) {
+            conj.AgregarRapido(itTags.Siguiente());
+            itTags.Avanzar()
+        }
+        return conj;
+    }
 
-void Driver::Mover(RUR robot, const Estacion &destino)
-{
- // TODO
-}
+    Nat Driver::CantInfraccionesIesimoRobotActivo(Nat i) const
+    {
+        itArreglo it = itArreglo(*robots);
+        int j = 0;
+        while (j < i) {
+            j = j + 1;
+            it.Avanzar();
+        }
+        return (it.Siguiente()).infracciones;
+    }
 
-void Driver::Inspeccion(const Estacion &e)
-{
-    // TODO
-}
+    RUR Driver::ElMasInfractor() const
+    {
+        itArreglo it = itArreglo(*robots);
+        RUR r = it.Siguiente().posEstacion.rur;
+        Nat inf = it.Siguiente().infracciones;
+
+        while (it.HaySiguiente()) {
+            if (it.Siguiente().infracciones > inf) {
+                r = it.Siguiente().posEstacion.rur;
+                inf = it.Siguiente().infracciones;
+            }
+            it.Avanzar();
+        }
+        return r;
+    }
+
+    void Driver::Entrar(const Conj<Caracteristica> &cs, const Estacion &estacionInicial)
+    {
+        ConjRapido* tags = new ConjRapido();
+        Conj<Caracteristica>::const_Iterador it = cs.CrearIt();
+
+        while (it.HaySiguiente()) {
+            tags.Agregar(it.Siguiente());
+            it.Avanzar()
+        }
+        ciudad.Entrar(tags, estacionInicial);
+    }
+
+    void Driver::Mover(RUR robot, const Estacion &destino)
+    {
+        ciudad.Mover(robot, destino);
+    }
+
+    void Driver::Inspeccion(const Estacion &e)
+    {
+        ciudad.Inspeccion(e);
+    }
+
+    template <typename T>
+    std::string to_str(const T& t)
+    {
+        std::stringstream ss;
+        ss << t;
+
+        return ss.str();
+    }
+
+    RestriccionTP& ParsearArbolSintactico(ArbolSintactico* expr) {
+        RestriccionTP* rest
+        if (expr->raiz == "&") {
+            rest = RestriccionTP::And(ParsearArbolSintactico(expr->izq),
+                                        ParsearArbolSintactico(expr->der));
+        } else if (expr->raiz == "|") {
+            rest = RestriccionTP::Or(ParsearArbolSintactico(expr->izq),
+                                        ParsearArbolSintactico(expr->der));
+        } else if (expr->raiz == "!") {
+            rest = RestriccionTP::Not(ParsearArbolSintactico(expr->izq));
+        } else {
+            rest = new RestriccionTP(expr->raiz);
+        }
+        return *rest;
+    }
 
 } // namespace aed2
--- Driver.h
+++ Driver.h
@@ -4,86 +4,90 @@
 #include "Tipos.h"
 #include "aed2/TiposBasicos.h"
 
-//#include ...
+#include "tp/Ciudad.h"
 
 namespace aed2 {
 
-class Driver
-{
+	class Driver
+	{
 	public:
 
-        Driver(const Conj<Estacion>& estacionesIniciales);
+		Driver(const Conj<Estacion>& estacionesIniciales);
 
 		~Driver();
 
-        /// Observadores Mapa //////////////////////////////////////////////////////////
+		/// Observadores Mapa //////////////////////////////////////////////////////////
 
-        Nat CantidadEstaciones() const;
+		Nat CantidadEstaciones() const;
 
-        // PRE: i < #estaciones()
-	    Estacion IesimaEstacion(Nat i) const;
+		// PRE: i < #estaciones()
+		Estacion IesimaEstacion(Nat i) const;
 
-        // PRE: e \in estaciones()
-        Nat CantidadDeSendasParaEstacion(const Estacion& e) const;
+		// PRE: e \in estaciones()
+		Nat CantidadDeSendasParaEstacion(const Estacion& e) const;
 
-        // PRE: e \in estaciones() \and i < # {e2:Estacion | conectadas?(e,e2)}
-        Estacion IesimaEstacionDeSenda(const Estacion& e, Nat i) const;
+		// PRE: e \in estaciones() \and i < # {e2:Estacion | conectadas?(e,e2)}
+		Estacion IesimaEstacionDeSenda(const Estacion& e, Nat i) const;
 
-        // PRE: e \in estaciones() \and i < # {e2:Estacion | conectadas?(e,e2)}
-        Restriccion IesimaRestriccionDeSenda(const Estacion& e1, Nat i) const;
+		// PRE: e \in estaciones() \and i < # {e2:Estacion | conectadas?(e,e2)}
+		Restriccion IesimaRestriccionDeSenda(const Estacion& e1, Nat i) const;
 
-        /// Armado Mapa ////////////////////////////////////////////////////////////////
+		/// Armado Mapa ////////////////////////////////////////////////////////////////
 
-        /*******************************************************
-         * VER DESCRIPCION DE LAS RESTRICCIONES EN Tipos.h !!! *
-         *******************************************************/
+		/*******************************************************
+		* VER DESCRIPCION DE LAS RESTRICCIONES EN Tipos.h !!! *
+		*******************************************************/
 
-        // PRE: e1 \in estaciones() \and e2 \in estaciones() \and \not conectadas?(e1,e2)
-        void AgregarSenda(const Estacion& e1, const Estacion& e2, Restriccion r);
+		// PRE: e1 \in estaciones() \and e2 \in estaciones() \and \not conectadas?(e1,e2)
+		void AgregarSenda(const Estacion& e1, const Estacion& e2, Restriccion r);
 
-        /// Observadores Ciudad //////////////////////////////////////////////////////////////
+		/// Observadores Ciudad //////////////////////////////////////////////////////////////
 
-        Nat CantidadRobotsActivos() const;
+		Nat CantidadRobotsActivos() const;
 
-        /// PRE: i < #robots()
-        RUR IesimoRobotActivo(Nat i) const;
+		/// PRE: i < #robots()
+		RUR IesimoRobotActivo(Nat i) const;
 
-        /// PRE: i < #robots()
-        Estacion EstacionActualIesimoRobotActivo(Nat i) const;
+		/// PRE: i < #robots()
+		Estacion EstacionActualIesimoRobotActivo(Nat i) const;
 
-        /// PRE: i < #robots()
-        Conj<Caracteristica> CaracteristicasIesimoRobotActivo(Nat i) const;
+		/// PRE: i < #robots()
+		Conj<Caracteristica> CaracteristicasIesimoRobotActivo(Nat i) const;
 
-        /// PRE: i < #robots()
-        Nat CantInfraccionesIesimoRobotActivo(Nat i) const;
+		/// PRE: i < #robots()
+		Nat CantInfraccionesIesimoRobotActivo(Nat i) const;
 
-        /// PRE: (\exists r:RUR) r \in robots() \and #infracciones(r) > 0
-        RUR ElMasInfractor() const;
+		/// PRE: (\exists r:RUR) r \in robots() \and #infracciones(r) > 0
+		RUR ElMasInfractor() const;
 
-        /// Acciones Ciudad ////////////////////////////////////////////////////////////
-        /// Al empezar a usar estas operaciones se asume que ya no se agregan sendas nuevas
+		/// Acciones Ciudad ////////////////////////////////////////////////////////////
+		/// Al empezar a usar estas operaciones se asume que ya no se agregan sendas nuevas
 
-        /// PRE: estacionInicial \in estaciones()
-        void Entrar(const Conj<Caracteristica> & cs, const Estacion& estacionInicial);
+		/// PRE: estacionInicial \in estaciones()
+		void Entrar(const Conj<Caracteristica> & cs, const Estacion& estacionInicial);
 
-        /// PRE: robot \in robots() \and destino \in estaciones()
-        void Mover(RUR robot, const Estacion& destino);
+		/// PRE: robot \in robots() \and destino \in estaciones()
+		void Mover(RUR robot, const Estacion& destino);
 
-        /// PRE: e \in estaciones()
-        void Inspeccion(const Estacion & e);
+		/// PRE: e \in estaciones()
+		void Inspeccion(const Estacion & e);
 
 	private:
 
-    /*********************************************************************
-     * TODO: Va a ser necesario instanciar privadamente un Modulo Ciudad *
-     * con el cuál interactuar. Además, pueden declarar todas las        *
-     * funciones auxiliares que les hagan falta.                         *
-     *********************************************************************/
-     Mapa mapa;
-     Ciudad* ciudad;
+		/*********************************************************************
+		* TODO: Va a ser necesario instanciar privadamente un Modulo Ciudad *
+		* con el cuál interactuar. Además, pueden declarar todas las        *
+		* funciones auxiliares que les hagan falta.                         *
+		*********************************************************************/
+		Mapa mapa;
+		Ciudad* ciudad;
 
-}; // class Driver
+		RestriccionTP& ParsearArbolSintactico(ArbolSintactico* expr);
 
+		template <typename T>
+		std::string to_str(const T& t);
+
+	}; // class Driver
 
 } // namespace aed2
 
